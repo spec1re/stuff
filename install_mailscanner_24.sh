521d664a91d423866b0b50e5822668e34231789d
spec1re <spec1re@users.noreply.github.com>
2018-09-10 15:08:34 +0200

    Add files via upload

---
+++ install_mailscanner_24.sh
@@ -0,0 +1,126 @@
+#!/bin/sh
+
+# *
+# * install_mailscanner_24.sh
+# *
+# * part of unofficial packages for pfSense(R) software
+# * Copyright (c) 2011-2017 Marcello Coutinho
+# * All rights reserved.
+# *
+# * Licensed under the Apache License, Version 2.0 (the "License");
+# * you may not use this file except in compliance with the License.
+# * You may obtain a copy of the License at
+# *
+# * http://www.apache.org/licenses/LICENSE-2.0
+# *
+# * Unless required by applicable law or agreed to in writing, software
+# * distributed under the License is distributed on an "AS IS" BASIS,
+# * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# * See the License for the specific language governing permissions and
+# * limitations under the License.
+
+ASSUME_ALWAYS_YES=YES
+export ASSUME_ALWAYS_YES
+
+if [ "$(cat /etc/version | cut -c 1-3)" == "2.4" ]; then
+prefix=https://raw.githubusercontent.com/marcelloc/Unofficial-pfSense-packages/master/pkg-mailscanner/files
+
+check_service_file=check_mailscanner_service.php
+fetch -q -o /root/$check_service_file $prefix/$check_service_file
+
+# /usr/local files
+
+for file in 	www/mailscanner_about.php pkg/mailscanner.conf.template pkg/mailscanner.xml pkg/mailscanner_antispam.xml \
+		pkg/mailscanner_attachments.xml pkg/mailscanner_report.xml pkg/mailscanner.inc pkg/mailscanner_alerts.xml \
+		pkg/mailscanner_antivirus.xml pkg/mailscanner_content.xml pkg/mailscanner_sync.xml \
+		www/shortcuts/pkg_mailscanner.inc bin/sa-updater-custom-channels.sh bin/sa-wrapper.pl
+do
+	echo "fetching  /usr/local/$file from github"
+	fetch -q -o /usr/local/$file $prefix/usr/local/$file
+done
+
+# Enable freebsd Repo
+repo_dir=/root/repo.bkp
+mkdir -p $repo_dir
+rm -f $repo_dir/*conf
+cp /usr/local/etc/pkg/repos/*conf $repo_dir
+sed -i "" -E "s/(FreeBSD.*enabled:) no/\1 yes/" /usr/local/etc/pkg/repos/*conf
+
+#fix permission
+chmod +x /usr/local/bin/sa-updater-custom-channels.sh
+chmod +x /usr/local/bin/sa-wrapper.pl
+
+# Install mailscanner package
+# pkg lock pkg
+pkg update
+pkg install mailscanner bash dcc-dccd spamassassin p7zip rsync p5-Geo-IP razor-agents p5-LWP-UserAgent-WithCache p5-Net-Patricia py36-pyzor p5-Encoding-FixLatin p5-Test-Pod p5-Mail-ClamAV p5-Mail-SPF p5-Module-Build p5-YAML
+
+# restore repository configuration state
+cp $repo_dir/*conf /usr/local/etc/pkg/repos/.
+
+#install services and menus
+php /root/check_mailscanner_service.php
+
+#install spamassassin-extremeshok_fromreplyto
+plugin_dir=/usr/local/etc/mail/spamassassin
+plugin_file=extremeshok_fromreplyto.zip
+
+if [ ! -d $plugin_dir/plugins ];then
+	mkdir -p $plugin_dir/plugins
+fi
+
+cd root
+
+fetch -o $plugin_file https://github.com/extremeshok/spamassassin-extremeshok_fromreplyto/archive/master.zip
+unzip -o $plugin_file
+cp spamassassin-extremeshok_fromreplyto-master/plugins/*pm $plugin_dir/plugins/
+cp spamassassin-extremeshok_fromreplyto-master/01_extremeshok_fromreplyto.cf $plugin_dir
+
+#install shorturl mailscanner plugin
+plugin_file=DecodeShortURLs.zip
+fetch -o $plugin_file https://github.com/smfreegard/DecodeShortURLs/archive/master.zip
+unzip -o $plugin_file
+cp DecodeShortURLs-master/*pm $plugin_dir
+cp DecodeShortURLs-master/*cf $plugin_dir
+
+#install 7z and pdf patch
+plugin_file=pdfid.zip 
+fetch -o $plugin_file http://didierstevens.com/files/software/pdfid_v0_2_1.zip
+unzip -o $plugin_file
+cp p*py /usr/local/bin/
+chmod +x /usr/local/bin/p*py
+#fix python path
+sed -i '.bak' "s@/usr/bin/env python@/usr/local/bin/python2@" /usr/local/bin/p*.py
+
+#install unofficial sigs for improving malware protection
+plugin_file=clamav-unofficial-sigs.zip
+fetch -o $plugin_file https://github.com/extremeshok/clamav-unofficial-sigs/archive/master.zip
+unzip -o $plugin_file
+script_file=/usr/local/sbin/clamav-unofficial-sigs.sh
+plugin_dir=clamav-unofficial-sigs
+cp ${plugin_dir}-master/clamav-unofficial-sigs.sh $script_file
+
+chmod +x $script_file
+sed -i '.bak' "s@!/bin/bash@!/usr/local/bin/bash@" $script_file
+for c_dir in /etc/$plugin_dir/ /var/log/$plugin_dir/
+do
+        if [ ! -d $c_dir ];then
+                mkdir $c_dir
+        fi
+done
+cp ${plugin_dir}-master/config/* /etc/$plugin_dir
+cp /etc/$plugin_dir/os.pfsense.conf /etc/$plugin_dir/os.conf
+sed -i '.bak' 's@clam_user=.*@clam_user="postfix"@' /etc/$plugin_dir/os.conf
+sed -i '.bak' 's@#user_configuration.*@user_configuration_complete="yes"@' /etc/$plugin_dir/user.conf
+
+# update spamassassin database
+rehash
+/usr/local/bin/sa-update -D
+
+fi
+
+#for PatchFile in ConfigDefs.pl.patch Message.pm.patch SweepContent.pm.patch
+#  do
+#  fetch -o - -q $prefix/$PatchFile | patch -N -b -p0
+#  done
+
---
+++ install_postfix_24.sh
@@ -0,0 +1,141 @@
+#!/bin/sh
+
+# *
+# * install_postfix_24.sh
+# *
+# * part of unofficial packages for pfSense(R) software
+# * Copyright (c) 2011-2017 Marcello Coutinho
+# * All rights reserved.
+# *
+# * Licensed under the Apache License, Version 2.0 (the "License");
+# * you may not use this file except in compliance with the License.
+# * You may obtain a copy of the License at
+# *
+# * http://www.apache.org/licenses/LICENSE-2.0
+# *
+# * Unless required by applicable law or agreed to in writing, software
+# * distributed under the License is distributed on an "AS IS" BASIS,
+# * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# * See the License for the specific language governing permissions and
+# * limitations under the License.
+
+ASSUME_ALWAYS_YES=YES
+export ASSUME_ALWAYS_YES
+
+if [ "$(cat /etc/version | cut -c 1-3)" == "2.4" ]; then
+prefix=https://raw.githubusercontent.com/marcelloc/Unofficial-pfSense-packages/master/pkg-postfix/files
+
+# /etc/inc files
+file=/etc/inc/priv/postfix.priv.inc
+fetch -q -o $file $prefix/$file
+
+check_service_file=check_postfix_service.php
+fetch -q -o /root/$check_service_file $prefix/$check_service_file
+
+# fix missing xmlrpc.inc
+fetch -q -o /etc/inc/xmlrpc.inc https://raw.githubusercontent.com/pfsense/pfsense/RELENG_2_3_5/src/etc/inc/xmlrpc.inc
+
+# /usr/local files
+
+#datatable files
+dtdir=www/vendor/datatable
+
+for share_dir in /usr/local/share/pfSense-pkg-postfix/ \
+		/usr/local/www/vendor/datatable/css \
+		/usr/local/www/vendor/datatable/js \
+		/usr/local/www/vendor/datatable/images
+   do 
+	if [ ! -d $share_dir ];then
+		mkdir -p $share_dir
+	fi
+   done
+
+for file in 	bin/adexport.pl pkg/postfix.inc pkg/postfix.xml pkg/postfix_acl.xml pkg/postfix_antispam.xml \
+		pkg/postfix_domains.xml pkg/postfix_recipients.xml pkg/postfix_sync.xml share/pfSense-pkg-postfix/info.xml \
+		www/postfix.php www/postfix_about.php www/postfix_queue.php www/postfix_recipients.php www/postfix_search.php \
+		www/postfix_view_config.php www/shortcuts/pkg_postfix.inc www/widgets/widgets/postfix.widget.php \
+		pkg/postfix_dkim.inc $dtdir/se-1.2.0.zip $dtdir/css/jquery.dataTables.min.css bin/sa-learn-pipe.sh\
+		$dtdir/js/jquery.dataTables.min.js www/postfix.sql.php bin/postwhite pkg/postfix_postwhite.template \
+		www/postfix_cloud_domains.php www/postfix_migrate_config.php pkg/postfix_dmarc.inc pkg/postfix_postfwd.inc
+ do
+	echo "fetching  /usr/local/$file from github"
+	fetch -q -o /usr/local/$file $prefix/usr/local/$file
+done
+
+fetch -q -o /root/postfix_report.php $prefix/root/postfix_report.php
+fetch -q -o /root/mail_report.sh $prefix/root/mail_report.sh
+
+#fix some permissions
+chmod +x /root/mail_report.sh
+chmod +x /usr/local/bin/postwhite
+chmod +x /root/postfix_report.php
+chmod +x /usr/local/bin/adexport.pl 
+chmod +x /usr/local/www/postfix.php
+chmod +x /usr/local/bin/sa-learn-pipe.sh
+
+#other minor fixes
+cp /usr/local/$dtdir/DataTables-1.10.13/images/sort_both.png /usr/local/$dtdir/images/sort_both.png
+cp /usr/local/$dtdir/DataTables-1.10.13/images/sort_asc.png /usr/local/$dtdir/images/sort_asc.png
+
+# Enable freebsd Repo
+repo_dir=/root/repo.bkp
+mkdir -p $repo_dir
+rm -f $repo_dir/*conf
+cp /usr/local/etc/pkg/repos/*conf $repo_dir
+#sed -i "" -E "s/(FreeBSD.*enabled:) no/\1 yes/" /usr/local/etc/pkg/repos/*conf
+sed -i "" -E "s/(FreeBSD.*enabled:) no/\1 yes/" /usr/local/share/pfSense/pkg/repos/pfSense-repo.conf
+sed -i "" -E "s/(FreeBSD.*enabled:) no/\1 yes/" /usr/local/etc/pkg/repos/FreeBSD.conf
+
+
+# Install postfix package 
+pkg update
+pkg install postfix libspf2 opendkim libmilter p5-perl-ldap python36 py36-postfix-policyd-spf-python postfix-postfwd opendmarc pflogsumm zip
+
+# restore repository configuration state
+#cp /root/pfSense.bkp.conf $repo2
+cp $repo_dir/*conf /usr/local/etc/pkg/repos/.
+
+#check some libs
+if [ ! -f /usr/local/lib/libmilter.so.5 ];then
+  ln -s /usr/local/lib/libmilter.so.7 /usr/local/lib/libmilter.so.5
+fi
+
+fetch -q -o /usr/local/etc/postfix/yahoo_static_hosts.txt https://raw.githubusercontent.com/stevejenkins/postwhite/master/yahoo_static_hosts.txt
+
+#install spf tools
+if [ -f master.zip ];then
+  mv master.zip master.old.zip
+fi
+
+if [ ! -d /usr/local/bin/spf-tools ];then
+   fetch https://github.com/jsarenik/spf-tools/archive/master.zip
+   unzip master.zip
+   mv spf-tools-master /usr/local/bin/spf-tools
+   rm -f master.zip
+fi
+#check postwhite
+if [ ! -f /usr/local/etc/postfix/postscreen_spf_whitelist.cidr ];then
+ /usr/local/bin/bash /usr/local/bin/postwhite
+fi
+
+#install services and menus
+php /root/check_postfix_service.php
+
+# unzip datagrid modules
+cd /usr/local/$dtdir
+/usr/bin/unzip -o se-1.2.0.zip 
+cd -
+
+echo "updating soft bounce message status on databases.."
+for a in /var/db/postfix/20*db
+
+do
+echo -n $a
+echo "update mail_noqueue set status='soft bounce' where status_info like '%Service currently unavailable%';" | sqlite3 $a
+echo " ok"
+done
+
+# pkg unlock pkg
+
+php /usr/local/www/postfix_migrate_config.php
+fi
